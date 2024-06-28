b7ca7d54d284269323aa3b3f1c1b0f04f3bb3d58
DoogieMac <xjoelee@gmail.com>
2024-06-29 02:44:50 +0900

    joeMac:240629-0244:

--- cargo_joe/src/main.rs
+++ cargo_joe/src/main.rs
@@ -1,26 +1,17 @@
 //
 // Hello~
 //
-use std::io;
-use rand::Rng;
+//use std::io;
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
-
-    /*
     let x = 5;
-    let y = 10;
-    println!("x = {x} and y + 2 = {}", y + 2);
-     */
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
 }
