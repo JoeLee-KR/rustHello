5046d893c336ef169b1f677c182652a8d2cde557
DoogieMac <xjoelee@gmail.com>
2024-07-30 17:28:45 +0900

    joeMac:240730-1728:

--- cargo_joe/src/main.rs
+++ cargo_joe/src/main.rs
@@ -2,16 +2,28 @@
 // Hello~
 //
 //use std::io;
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
 
 fn main() {
-    let x = 5;
+    let mut s = String::from("hello world");
+    println!("the first word is: {}", s);
 
-    let x = x + 1;
+    let word = first_word(&s);
+    //s.clear(); // 에러!
+    println!("the second word is: {}", word);
+    s.clear(); // 정상!
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
-    println!("The value of x is: {x}");
+    let a = [11, 12, 13, 14, 15];
+    let slice = &a[1..4];
+    println!("slice test, a is {}, slice is {}", a.len(), slice.len());
+    assert_eq!(slice, &[12, 13, 14]);
 }
